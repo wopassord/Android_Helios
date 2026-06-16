54bf5360a4b792441ca7e58aaca538a4bce819a7
Facundo Arito <aritofacundo@gmail.com>
2026-06-16 17:57:22 +0200

    Fixed errors

--- app/src/main/java/com/example/android_helios/MainActivity.java
+++ app/src/main/java/com/example/android_helios/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.android_helios;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +14,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometer;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +29,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        if (sensorManager != NULL) {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         }
-        if (sensorManager = NULL){
-            Log.e("The device does not have an accelerometer");
+        if (accelerometer == null) {
+            Log.e("MainActivity", "The device does not have an accelerometer");
         }
     }
 
     @Override
     protected void onResume() {
-        //this function enables resuming the app's functioning, once it's reopened  by the user
         super.onResume();
-
-        if (accelerometer != NULL) {
+        if (accelerometer != null) {
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
     @Override
     protected void onPause() {
-        //this function disables the app's functioning once the user leaves it
         super.onPause();
-
-        if(accelerometer != NULL) {
+        if (accelerometer != null) {
             sensorManager.unregisterListener(this);
         }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
